a86075dfcb86c9d053bc64494d00f48c98faab92
fivo-arnande11 <fivo.arnande@gmail.com>
2022-09-01 15:56:58 +0700

    Praktikum section7 task1

---
+++ 07_Branching - Looping - Function/praktikum/praktikum1_section7.dart
@@ -0,0 +1,18 @@
+void main() {
+  
+  print("Terdapat sebuah nilai:");
+  double nilai = 71;
+ 
+  if (nilai > 70 ){
+    print("Jika nilai > 70 maka : A");
+  } else if (nilai > 40){
+    print("Jika nilai > 40 maka : B");
+  } else if (nilai > 0){
+    print("Jika nilai > 0 maka : C");
+  }
+   else {
+    print("Teks Kosong, masukkan nilai");
+  }
+  
+    
+  }
