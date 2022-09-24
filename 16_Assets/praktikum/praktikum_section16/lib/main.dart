6a742466b4dfbd20aeb05ad16afd37c669b43220
fivo-arnande11 <fivo.arnande@gmail.com>
2022-09-24 16:38:52 +0700

    praktikum section16

--- 16_Assets/praktikum/praktikum_section16/lib/main.dart
+++ 16_Assets/praktikum/praktikum_section16/lib/main.dart
@@ -1,64 +1,176 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Asset GridView"),
-        ),
-        body: Column(children: <Widget>[
-          Flexible(
-            child: GridView.count(
-              crossAxisCount: 2,
-              children: <Widget> [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.all(10),
-                  child: Image(
-                    image: AssetImage(
-                      "assets/aokiji.jpg",
-                    ),
-                  ),
+    return const MaterialApp(
+      title: 'Image/Detail Demo',
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+      ),
+      body: GridView.count(
+        padding: const EdgeInsets.all(10),
+        mainAxisSpacing: 2,
+        crossAxisSpacing: 2,
+        crossAxisCount: 2,
+        children: [
+          FittedBox(
+            fit: BoxFit.fill,
+            child: GestureDetector(
+              child: Hero(
+                tag: 'image1',
+                child: Image.network(
+                  'https://picsum.photos/id/237/200/300'
                 ),
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  margin: EdgeInsets.all(10),
-                  child: Image(
-                    image: AssetImage(
-                      "assets/fujitora.jpg",
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.all(10),
-                  child: Image(
-                    image: AssetImage(
-                      "assets/shirohige.jpg",
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.all(10),
-                  child: Image(
-                    image: AssetImage(
-                      "assets/akainu.jpg",
-                    ),
-                  ),
-                )
-              ],
+              ),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) {
+                  return const DetailScreen1();
+                }));
+              },
             ),
           ),
-        ]),
+          FittedBox(
+            fit: BoxFit.fill,
+            child: GestureDetector(
+              child: Hero(
+                tag: 'image2',
+                child: Image.network(
+                  'https://picsum.photos/id/240/200/300'
+                ),
+              ),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) {
+                  return const DetailScreen2();
+                }));
+              },
+            ),
+          ),
+          FittedBox(
+            fit: BoxFit.fill,
+            child: GestureDetector(
+              child: Hero(
+                tag: 'image3',
+                child: Image.network(
+                  'https://picsum.photos/id/260/200/300'
+                ),
+              ),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) {
+                  return const DetailScreen3();
+                }));
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DetailScreen1 extends StatelessWidget {
+  const DetailScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Screen"),
+      ),
+      body: GestureDetector(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width,
+          child: FittedBox(
+            fit: BoxFit.fill,
+            child: Hero(
+              tag: 'image1',
+              child: Image.network(
+                'https://picsum.photos/id/237/200/300'
+              ),
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen2 extends StatelessWidget {
+  const DetailScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Screen"),
+      ),
+      body: GestureDetector(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width,
+          child: FittedBox(
+            fit: BoxFit.fill,
+            child: Hero(
+              tag: 'image2',
+              child: Image.network(
+                'https://picsum.photos/id/240/200/300'
+              ),
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen3 extends StatelessWidget {
+  const DetailScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Screen"),
+      ),
+      body: GestureDetector(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width,
+          child: FittedBox(
+            fit: BoxFit.fill,
+            child: Hero(
+              tag: 'image3',
+              child: Image.network(
+                'https://picsum.photos/id/260/200/300',
+              ),
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
       ),
     );
   }
